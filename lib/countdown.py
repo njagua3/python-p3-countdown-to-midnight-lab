fa21de0a4829d23520020bdbeb23fc8785c83e21
njagua <njagua.gma@gmail.com>
2024-08-31 10:45:00 +0300

    passing tests

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
 # your code goes here!
+import time
+def countdown(i):
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print('HAPPY NEW YEAR!')
+
+
+def countdown_with_sleep(i):
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        time.sleep(1)
+        i -= 1
+    print('HAPPY NEW YEAR!')
